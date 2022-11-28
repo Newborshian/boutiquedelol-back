0d0e87ca1637c4eb1681ebc34f25887e00ea8ad3
Newborshian <christopherbouthemy2014@gmail.com>
2022-11-28 16:21:36 +0100

    Créer les entity

--- src/main/java/com/example/superboutiquedelolback/entity/ProductEntity.java
+++ src/main/java/com/example/superboutiquedelolback/entity/ProductEntity.java
@@ -1,4 +1,87 @@
 package com.example.superboutiquedelolback.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
 public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "image")
+    private String image;
+
+    @Column(name = "lore")
+    private String lore;
+
+    @Column(name = "price")
+    private Double price;
+
+    @Column(name = "available")
+    private Boolean available;
+
+    @Column(name = "type")
+    private String type;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getLore() {
+        return lore;
+    }
+
+    public void setLore(String lore) {
+        this.lore = lore;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
--- src/main/java/com/example/superboutiquedelolback/entity/UserEntity.java
+++ src/main/java/com/example/superboutiquedelolback/entity/UserEntity.java
@@ -1,4 +1,76 @@
 package com.example.superboutiquedelolback.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "mail")
+    private String mail;
+
+    @Column(name = "role")
+    private String role;
+
+    @Column(name = "status")
+    private Boolean status;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
 }
